175cc19fa76d73a5dd998cd6494fbab737dcb28b
DianNoviyanto <80396346+DianNoviyanto@users.noreply.github.com>
2021-03-24 11:24:46 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form Pendaftaran Anggota</title>
+    <title>Form Update Produk</title>
     <!-- Load file CSS Bootstrap offline -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
